b11cb084f72dafb2b8116867b61d3ab847b76966
Swaniket <swaniketchowdhury@gmail.com>
2022-01-08 18:16:05 +0530

    Added XOR alternative in only odd

--- 02. Array/07_onlyOdd.py
+++ 02. Array/07_onlyOdd.py
@@ -1,12 +1,23 @@
 def onlyOdd(lst):
-    pass
+    result = None
+    for i in lst:
+        count = lst.count(i)
+        if count%2 != 0:
+            result = i
+            break
+    return result
     
-
+def onlyOddXOR(lst):
+    result = 0
+    for i in lst:
+        result = result ^ i
+    return result
 
 if __name__ == "__main__":
-    lst = [10,30,30,10,30,30,20] #OP: 20
+    # lst = [10,30,30,10,30,30,20] #OP: 20
     # lst = [10,10,10,10,10,20,20] #OP: 10
     # lst = [10,10,20,30,30,20,40] #OP: 40
-    # lst = [10] #OP: 10
+    lst = [10] #OP: 10
 
     print(onlyOdd(lst))
+    print(onlyOddXOR(lst))
