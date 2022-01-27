ebeda952abccf071b1fb54f402771478a69951f4
Swaniket <swaniketchowdhury@gmail.com>
2022-01-27 10:59:02 +0530

    Implemented delete node & delete bst methods for BST

--- 07. Binary Search Tree/01_BST.py
+++ 07. Binary Search Tree/01_BST.py
@@ -65,7 +65,59 @@ def levelOrderTraversal(rootNode): # Time Complexity -> O(n), Space Complexity -
             if root.value.rightChild is not None:
                 customQueue.enqueue(root.value.rightChild)
 
+# Search for a node in BST
+def searchBST(rootNode, nodeValue): # Time Complexity -> O(logN), Space Complexity -> O(logN)
+    if rootNode.data == nodeValue:
+        print("The value is found")
+    elif nodeValue < rootNode.data:
+        if rootNode.leftChild.data == nodeValue:
+            print("The value is found")
+        else:
+            searchBST(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild.data == nodeValue:
+            print("The value is found")
+        else:
+            searchBST(rootNode.rightChild, nodeValue)
+    return "The value is not found"
 
+# Helper for delete - Find the successor - min value in the right subtree
+def minValueNode(BSTNode):
+    current = BSTNode
+    while current.leftChild is not None:
+        current = current.next
+    return current
+
+# Delete a node from BST
+def deleteNode(rootNode, nodeValue): # Time Complexity -> O(logN), Space Complexity -> O(logN)
+    if rootNode is None:
+        return rootNode
+    if nodeValue < rootNode.data:
+        rootNode.leftChild = deleteNode(rootNode.leftChild, nodeValue)
+    elif nodeValue > rootNode.data:
+        rootNode.rightChild = deleteNode(rootNode.rightChild, nodeValue)
+    else:
+        # Delete for only 1 child
+        if rootNode.leftChild is None:
+            temp = rootNode.rightChild
+            rootNode = None
+            return temp
+        if rootNode.rightChild is None:
+            temp = rootNode.leftChild
+            rootNode = None
+            return temp
+        # Delete a node which has 2 children
+        temp = minValueNode(rootNode.rightChild)
+        rootNode.data = temp.data
+        rootNode.rightChild = deleteNode(rootNode.rightChild, temp.data)
+    return rootNode
+
+# Delete the entire BST
+def deleteBST(rootNode): # Time Complexity -> O(1), Space Complexity -> O(1)
+    rootNode.data = None
+    rootNode.leftChild = None
+    rootNode.rightChild = None
+    return "The BST has been successfully deleted"
 
 
 if __name__ == "__main__":
@@ -87,4 +139,13 @@ if __name__ == "__main__":
     # preOrderTraversal(newBST)
     # inOrderTraversal(newBST)
     # postOrderTraversal(newBST)
+    # levelOrderTraversal(newBST)
+
+    # searchBST(newBST, 40)
+
+    # deleteNode(newBST, 20)
+    # deleteNode(newBST, 100)
+
+    print(deleteBST(newBST))
+
     levelOrderTraversal(newBST)
