87744cd4c5f5832f2e45fccc5d938b1e3f1f80d0
Swaniket <swaniketchowdhury@gmail.com>
2022-01-29 22:15:57 +0530

    Added chainning collision handle

--- 09. Hashing/01_hashMap.py
+++ 09. Hashing/01_hashMap.py
@@ -7,8 +7,8 @@
 
 class HashTable:
     def __init__(self) -> None:
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
 
     def getHash(self, key) -> int:
         h = 0
@@ -18,25 +18,43 @@ class HashTable:
 
     def __setitem__(self, key, value):
         h = self.getHash(key)
-        self.arr[h] = value
+        found = False
+        # Updation
+        for index, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][index] = (key, value)
+                found = True
+                break
+        # First time insertion - When the key doesn't exists
+        if not found:
+            self.arr[h].append((key, value)) 
 
     def __getitem__(self, key):
         h = self.getHash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
 
     def __delitem__(self, key):
         h = self.getHash(key)
-        self.arr[h] = None
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][index]
 
 if __name__ == "__main__":
     ht = HashTable()
     
     ht['march 6'] = 130
-    ht['march 1'] = 10
-    ht['dec 17'] = 27
+    ht['march 6'] = 78
+    ht['march 8'] = 67
+    ht['march 9'] = 4
+    ht['march 17'] = 459
 
-    del ht['march 6']
+    # del ht['march 6']
 
-    # print(ht.arr)
+    print(ht.arr)
 
-    print(ht['march 6'])
+    # print(ht['march 17'])
+    del ht['march 17']
+    
+    print(ht.arr)
